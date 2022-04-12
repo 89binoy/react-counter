39ae15ce306318619fc3ade177c5dcf449eef04e
Binoy Saha <binoy.saha01@northsouth.edu>
2022-04-12 23:57:22 +0600

    simple increment decrement app by react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+const [count,setCount]=useState(0);
+  const increment=(data)=>{
+    setCount (count+data);
+  }
+  const decrement=(data) =>{
+    setCount (count-data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Value of the counter is: {count}</h2>
+
+      <button onClick={() => increment(1)}>increment 1</button>
+      <button onClick={() => increment(5)}>increment 5</button>
+      <button onClick={() => increment(10)}>increment 10</button>
+      <button onClick={() => increment(100)}>increment 100</button>
+      <br></br>
+      <button onClick={( )=> decrement(1)}>decrement 1</button>
+      <button onClick={() => decrement(5)}>decrement 5</button>
+      <button onClick={() => decrement(10)}>decrement 10</button>
+      <button onClick={() => decrement(100)}>decrement 100</button>
+
     </div>
   );
 }
